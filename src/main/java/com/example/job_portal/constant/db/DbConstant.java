037b79f79ff882e1af0fcf7f8cc6cef71a52c78f
tusher <md.tanvir.hossain.tusher@gmail.com>
2024-10-30 17:04:07 +0600

    Add Role entity to determine multiple roles of the users

--- src/main/java/com/example/job_portal/constant/db/DbConstant.java
+++ src/main/java/com/example/job_portal/constant/db/DbConstant.java
@@ -16,33 +16,33 @@ public class DbConstant {
     }
 
     public static class DbUniversity extends DbCommon {
-        public static final String TABLE_NAME = "tbl_university";
+        public static final String TABLE_NAME = "universities";
         public static final String UNIVERSITY_NAME = "university_name";
         public static final String DEGREE = "degree";
         public static final String PASSING_YEAR = "passing_year";
     }
 
     public static class DbSkill extends DbCommon {
-        public static final String TABLE_NAME = "tbl_university";
+        public static final String TABLE_NAME = "skills";
         public static final String SKILL_NAME = "skill_name";
     }
 
     public static class DbCV extends DbCommon {
-        public static final String TABLE_NAME = "tbl_cv";
+        public static final String TABLE_NAME = "cvs";
         public static final String CV_FORMAT = "cv_format";
         public static final String CV_SIZE = "cv_size";
         public static final String CV_URL = "cv_url";
     }
 
     public static class DbJob extends DbCommon {
-        public static final String TABLE_NAME = "tbl_job";
+        public static final String TABLE_NAME = "jobs";
         public static final String JOB_DESCRIPTION = "job_description";
         public static final String SALARY = "salary";
         public static final String IS_FRAUDULENT = "is_fraudulent";
     }
 
     public static class DbCompany extends DbCommon {
-        public static final String TABLE_NAME = "tbl_company";
+        public static final String TABLE_NAME = "companies";
         public static final String COMPANY_NAME = "company_name";
         public static final String COMPANY_LOCATION = "company_location";
         public static final String COMPANY_TYPE = "company_type";
@@ -50,7 +50,11 @@ public class DbConstant {
     }
 
     public static class DbMyCompany extends DbCommon {
-        public static final String TABLE_NAME = "tbl_my_company";
+        public static final String TABLE_NAME = "my_companies";
     }
 
+    public static class DbRole extends DbCommon {
+        public static final String TABLE_NAME = "roles";
+        public static final String ROLE = "role";
+    }
 }
--- src/main/java/com/example/job_portal/entity/Role.java
+++ src/main/java/com/example/job_portal/entity/Role.java
@@ -1,4 +1,28 @@
 package com.example.job_portal.entity;
 
+import com.example.job_portal.constant.db.DbConstant.DbRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = DbRole.TABLE_NAME)
 public class Role extends AuditInfo {
+
+    @Column(name = DbRole.ROLE)
+    private String role;
+
+    @ManyToOne(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<User> users;
 }
