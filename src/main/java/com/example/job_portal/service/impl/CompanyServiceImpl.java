59c32a58285ffab9ffd22a210724aeb6760603af
tusher <md.tanvir.hossain.tusher@gmail.com>
2024-10-31 14:42:53 +0600

    Add @Transactional annotation in the save method in CompanyServiceImpl

--- src/main/java/com/example/job_portal/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/example/job_portal/service/impl/CompanyServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.job_portal.entity.Company;
 import com.example.job_portal.repository.CompanyRepository;
 import com.example.job_portal.service.CompanyService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CompanyServiceImpl implements CompanyService {
@@ -15,6 +16,7 @@ public class CompanyServiceImpl implements CompanyService {
     }
 
     @Override
+    @Transactional
     public void save(Company company) {
         companyRepository.save(company);
     }
